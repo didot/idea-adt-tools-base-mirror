tadfisher/platform_tools_base

Create standalone NDK app
Change-Id: I550e48b2a873d773db98671caaaa8761e6c7fa55

build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -1810,10 +1810,15 @@ public abstract class BasePlugin {
         // Add dependencies on NDK tasks if NDK plugin is applied.
         if (extension.getUseNewNativePlugin()) {
             NdkPlugin ndkPlugin = project.plugins.getPlugin(NdkPlugin.class)
-            packageApp.dependsOn (ndkPlugin.getNdkTasks(variantConfig))
+            packageApp.dependsOn ndkPlugin.getBinaries(variantConfig)
         } else {
             packageApp.dependsOn variantData.ndkCompileTask
         }
+
+        if (extension.ndkLib != null) {
+            packageApp.dependsOn extension.ndkLib.getBinaries(variantConfig)
+        }
+
 
         packageApp.plugin = this
 
@@ -1830,13 +1835,17 @@ public abstract class BasePlugin {
             Set<File> set = Sets.newHashSet()
             if (extension.getUseNewNativePlugin()) {
                 NdkPlugin ndkPlugin = project.plugins.getPlugin(NdkPlugin.class)
-                set.addAll(ndkPlugin.getOutputDirectory(variantConfig))
+                set.addAll(ndkPlugin.getOutputDirectories(variantConfig))
             } else {
                 set.addAll(variantData.ndkCompileTask.soFolder)
             }
             set.addAll(variantData.renderscriptCompileTask.libOutputDir)
             set.addAll(variantConfig.libraryJniFolders)
             set.addAll(variantConfig.jniLibsList)
+
+            if (extension.ndkLib != null) {
+                set.addAll(extension.ndkLib.getOutputDirectories(variantConfig))
+            }
 
             if (variantConfig.mergedFlavor.renderscriptSupportMode) {
                 File rsLibs = androidBuilder.getSupportNativeLibFolder()

didot/idea-adt-tools-base-mirror

build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -1886,9 +1886,13 @@ public abstract class BasePlugin {
             // Add dependencies on NDK tasks if NDK plugin is applied.
             if (extension.getUseNewNativePlugin()) {
                 NdkPlugin ndkPlugin = project.plugins.getPlugin(NdkPlugin.class)
-                packageApp.dependsOn(ndkPlugin.getNdkTasks(config))
+                packageApp.dependsOn ndkPlugin.getBinaries(config)
             } else {
                 packageApp.dependsOn variantData.ndkCompileTask
+            }
+
+            if (extension.ndkLib != null) {
+                packageApp.dependsOn extension.ndkLib.getBinaries(config)
             }
 
             packageApp.plugin = this
@@ -1907,13 +1911,16 @@ public abstract class BasePlugin {
                 Set<File> set = Sets.newHashSet()
                 if (extension.getUseNewNativePlugin()) {
                     NdkPlugin ndkPlugin = project.plugins.getPlugin(NdkPlugin.class)
-                    set.addAll(ndkPlugin.getOutputDirectory(config))
+                    set.addAll(ndkPlugin.getOutputDirectories(config))
                 } else {
                     set.addAll(variantData.ndkCompileTask.soFolder)
                 }
                 set.addAll(variantData.renderscriptCompileTask.libOutputDir)
                 set.addAll(config.libraryJniFolders)
                 set.addAll(config.jniLibsList)
+                if (extension.ndkLib != null) {
+                    set.addAll(extension.ndkLib.getOutputDirectories(config))
+                }
 
                 if (config.mergedFlavor.renderscriptSupportMode) {
                     File rsLibs = androidBuilder.getSupportNativeLibFolder()