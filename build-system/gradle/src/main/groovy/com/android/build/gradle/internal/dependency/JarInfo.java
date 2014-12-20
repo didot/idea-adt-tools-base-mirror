tadfisher/platform_tools_base

Fix dependency management.
The previous change that fixed local jar support broke
support for transitive dependencies on jar artifacts/modules
through (android) library dependencies.

The causes where:
- when traversing the dependency graph, we keep a cache
  of modules already processed (and their dependencies) so
  that we can avoid processing it when finding it more
  than once in the same graph.
- We go through 2+ graphs (compile, package, tests ...)
- This cache only handled android libraries, and only
  their library dependencies, not their jar dependencies.
- Jars were only handled as the graph was traversed and copied
  into a flat list. This means interrupting going through a
  graph due to a cached library would skip its jars.
- Somehow the old code for local jars just looked at all
  the dependencies as files and looked of unknown ones and
  considered them local.

The previous change basically broke the last point. So
this change now does:

- process jars the same way we process libraries, with a cache
  that includes a module and its dependencies.
- cached libraries also include their jar dependencies.
- we flatten the jars at the end into the VariantDependency
  object.

This change also compares the compile and package composite
configuration object to ensure that there is no provided-only
or package-only dependency on aar libraries.
It properly handles provided-only and package-only jar
dependencies (modules/artifacts) and reports all errors
through the new SyncIssue mechanism.

Finally there are several more tests that test combinations
of provided/compile/package dependencies on local/jar/libraries
directly or indirectly. The test checks both the model and the
build output.

Change-Id: Ic7e0b4c42ad364817e2a281c213e7a4565e3ace7

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/dependency/JarInfo.java
@@ -1,0 +1,96 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.internal.dependency;
+
+import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
+import com.android.builder.dependency.JarDependency;
+import com.android.builder.model.MavenCoordinates;
+import com.google.common.collect.Lists;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Information about a jar dependency as well as its transitive dependency.
+ */
+public class JarInfo {
+
+    @NonNull
+    private final File jarFile;
+
+    private boolean compiled = false;
+    private boolean packaged = false;
+
+    @NonNull
+    final List<JarInfo> dependencies = Lists.newArrayList();
+
+    @Nullable
+    private final MavenCoordinates resolvedCoordinates;
+
+    public JarInfo(
+            @NonNull File jarFile,
+            @Nullable MavenCoordinates resolvedCoordinates,
+            @NonNull List<JarInfo> dependencies) {
+        this.jarFile = jarFile;
+        this.resolvedCoordinates = resolvedCoordinates;
+        this.dependencies.addAll(dependencies);
+    }
+
+    public void setCompiled(boolean compiled) {
+        this.compiled = compiled;
+    }
+
+    public void setPackaged(boolean packaged) {
+        this.packaged = packaged;
+    }
+
+    @NonNull
+    public File getJarFile() {
+        return jarFile;
+    }
+
+    @Nullable
+    public MavenCoordinates getResolvedCoordinates() {
+        return resolvedCoordinates;
+    }
+
+    @NonNull
+    public List<JarInfo> getDependencies() {
+        return dependencies;
+    }
+
+    @NonNull
+    public JarDependency createJarDependency() {
+        return new JarDependency(
+                jarFile,
+                compiled,
+                packaged,
+                true /*proguarded*/,
+                resolvedCoordinates
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "JarInfo{" +
+                "mJarFile=" + jarFile +
+                ", mDependencies=" + dependencies +
+                ", mResolvedCoordinates=" + resolvedCoordinates +
+                '}';
+    }
+}

didot/idea-adt-tools-base-mirror

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/dependency/JarInfo.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.internal.dependency;
+
+import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
+import com.android.builder.dependency.JarDependency;
+import com.android.builder.model.MavenCoordinates;
+import com.google.common.collect.Lists;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Information about a jar dependency as well as its transitive dependencies.
+ */
+public class JarInfo {
+
+    @NonNull
+    private final File jarFile;
+
+    private boolean compiled = false;
+    private boolean packaged = false;
+
+    @NonNull
+    final List<JarInfo> dependencies = Lists.newArrayList();
+
+    @Nullable
+    private final MavenCoordinates resolvedCoordinates;
+
+    public JarInfo(
+            @NonNull File jarFile,
+            @Nullable MavenCoordinates resolvedCoordinates,
+            @NonNull List<JarInfo> dependencies) {
+        this.jarFile = jarFile;
+        this.resolvedCoordinates = resolvedCoordinates;
+        this.dependencies.addAll(dependencies);
+    }
+
+    public void setCompiled(boolean compiled) {
+        this.compiled = compiled;
+    }
+
+    public void setPackaged(boolean packaged) {
+        this.packaged = packaged;
+    }
+
+    @NonNull
+    public File getJarFile() {
+        return jarFile;
+    }
+
+    @Nullable
+    public MavenCoordinates getResolvedCoordinates() {
+        return resolvedCoordinates;
+    }
+
+    @NonNull
+    public List<JarInfo> getDependencies() {
+        return dependencies;
+    }
+
+    @NonNull
+    public JarDependency createJarDependency() {
+        return new JarDependency(
+                jarFile,
+                compiled,
+                packaged,
+                true /*proguarded*/,
+                resolvedCoordinates
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "JarInfo{" +
+                "jarFile=" + jarFile +
+                ", compiled=" + compiled +
+                ", packaged=" + packaged +
+                ", dependencies=" + dependencies +
+                ", resolvedCoordinates=" + resolvedCoordinates +
+                '}';
+    }
+}