tadfisher/platform_tools_base

Add getRuntimeJavaResourcesFolder to BaseArtifact.
This way we can start putting it on the classpath when running unit
tests from AS.

Change-Id: I2abf0b61797e5f24c08d3640b66b76a87ed357b4

build-system/builder-model/src/main/java/com/android/builder/model/BaseArtifact.java
@@ -57,6 +57,13 @@ public interface BaseArtifact {
     File getClassesFolder();
 
     /**
+     * Returns the folder containing resource files that classes form this artifact expect to find
+     * on the classpath.
+     */
+    @NonNull
+    File getRuntimeJavaResourcesFolder();
+
+    /**
      * Returns the resolved dependencies for this artifact. This is a composite of all the
      * dependencies for that artifact: default config + build type + flavor(s).s
      *

build-system/gradle-core/src/main/groovy/com/android/build/gradle/BaseExtension.groovy
@@ -526,10 +526,11 @@ public abstract class BaseExtension {
             @NonNull Iterable<String> ideSetupTaskNames,
             @NonNull Configuration configuration,
             @NonNull File classesFolder,
+            @NonNull File runtimeJavaResourceFolder,
             @Nullable SourceProvider sourceProvider) {
         extraModelInfo.registerJavaArtifact(name, variant, assembleTaskName,
                 javaCompileTaskName, generatedSourceFolders, ideSetupTaskNames,
-                configuration, classesFolder, sourceProvider)
+                configuration, classesFolder, runtimeJavaResourceFolder, sourceProvider)
     }
 
     public void registerMultiFlavorSourceProvider(

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/ExtraModelInfo.java
@@ -222,6 +222,7 @@ public class ExtraModelInfo {
             @NonNull Iterable<String> ideSetupTaskNames,
             @NonNull Configuration configuration,
             @NonNull File classesFolder,
+            @NonNull File runtimeJavaResourecesFolder,
             @Nullable SourceProvider sourceProvider) {
         ArtifactMetaData artifactMetaData = extraArtifactMap.get(name);
         if (artifactMetaData == null) {
@@ -235,8 +236,9 @@ public class ExtraModelInfo {
 
         JavaArtifact artifact = new JavaArtifactImpl(
                 name, assembleTaskName, javaCompileTaskName, ideSetupTaskNames,
-                generatedSourceFolders, classesFolder, new ConfigurationDependencies(configuration),
-                sourceProvider, null);
+                generatedSourceFolders, classesFolder, runtimeJavaResourecesFolder,
+                new ConfigurationDependencies(configuration), sourceProvider, null);
+
         extraJavaArtifacts.put(variant.getName(), artifact);
     }
 

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/model/AndroidArtifactImpl.java
@@ -69,14 +69,16 @@ public class AndroidArtifactImpl extends BaseArtifactImpl implements AndroidArti
             @NonNull List<File> generatedSourceFolders,
             @NonNull List<File> generatedResourceFolders,
             @NonNull File classesFolder,
+            @NonNull File runtimeJavaResourcesFolder,
             @NonNull Dependencies dependencies,
             @Nullable SourceProvider variantSourceProvider,
             @Nullable SourceProvider multiFlavorSourceProviders,
             @Nullable Set<String> abiFilters,
             @NonNull Map<String,ClassField> buildConfigFields,
             @NonNull Map<String,ClassField> resValues) {
-        super(name, assembleTaskName, compileTaskName, classesFolder, dependencies,
-                variantSourceProvider, multiFlavorSourceProviders, generatedSourceFolders);
+        super(name, assembleTaskName, compileTaskName, classesFolder, runtimeJavaResourcesFolder,
+                dependencies, variantSourceProvider, multiFlavorSourceProviders,
+                generatedSourceFolders);
 
         this.outputs = outputs;
         this.isSigned = isSigned;

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/model/JavaArtifactImpl.java
@@ -47,6 +47,7 @@ public class JavaArtifactImpl extends BaseArtifactImpl implements JavaArtifact, 
                 javaArtifact.getIdeSetupTaskNames(),
                 javaArtifact.getGeneratedSourceFolders(),
                 javaArtifact.getClassesFolder(),
+                javaArtifact.getRuntimeJavaResourcesFolder(),
                 DependenciesImpl.cloneDependenciesForJavaArtifacts(javaArtifact.getDependencies()),
                 variantSP != null ? SourceProviderImpl.cloneProvider(variantSP) : null,
                 flavorsSP != null ? SourceProviderImpl.cloneProvider(flavorsSP) : null);
@@ -58,10 +59,12 @@ public class JavaArtifactImpl extends BaseArtifactImpl implements JavaArtifact, 
                             @NonNull Iterable<String> ideSetupTaskNames,
                             @NonNull Collection<File> generatedSourceFolders,
                             @NonNull File classesFolder,
+                            @NonNull File runtimeJavaResourcesFolder,
                             @NonNull Dependencies dependencies,
                             @Nullable SourceProvider variantSourceProvider,
                             @Nullable SourceProvider multiFlavorSourceProviders) {
-        super(name, assembleTaskName, compileTaskName, classesFolder, dependencies,
+        super(name, assembleTaskName, compileTaskName, classesFolder, runtimeJavaResourcesFolder,
+                dependencies,
                 variantSourceProvider, multiFlavorSourceProviders, generatedSourceFolders);
         this.ideSetupTaskNames = Sets.newHashSet(ideSetupTaskNames);
     }

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/UnitTestingModelTest.groovy
@@ -77,6 +77,11 @@ android {
             // No per-variant source code.
             assertThat(unitTestArtifact.variantSourceProvider).isNull()
             assertThat(unitTestArtifact.multiFlavorSourceProvider).isNull()
+
+            assertThat(variant.mainArtifact.runtimeJavaResourcesFolder.path)
+                    .endsWith("intermediates/javaResources/" + variant.name)
+            assertThat(unitTestArtifact.runtimeJavaResourcesFolder.path)
+                    .endsWith("intermediates/javaResources/test/" + variant.name)
         }
 
         def sourceProvider = model.defaultConfig

didot/idea-adt-tools-base-mirror

build-system/builder-model/src/main/java/com/android/builder/model/BaseArtifact.java
@@ -57,6 +57,13 @@ public interface BaseArtifact {
     File getClassesFolder();
 
     /**
+     * Returns the folder containing resource files that classes form this artifact expect to find
+     * on the classpath.
+     */
+    @NonNull
+    File getJavaResourcesFolder();
+
+    /**
      * Returns the resolved dependencies for this artifact. This is a composite of all the
      * dependencies for that artifact: default config + build type + flavor(s).s
      *

build-system/gradle-core/src/main/groovy/com/android/build/gradle/BaseExtension.groovy
@@ -526,10 +526,11 @@ public abstract class BaseExtension {
             @NonNull Iterable<String> ideSetupTaskNames,
             @NonNull Configuration configuration,
             @NonNull File classesFolder,
+            @NonNull File javaResourceFolder,
             @Nullable SourceProvider sourceProvider) {
         extraModelInfo.registerJavaArtifact(name, variant, assembleTaskName,
                 javaCompileTaskName, generatedSourceFolders, ideSetupTaskNames,
-                configuration, classesFolder, sourceProvider)
+                configuration, classesFolder, javaResourceFolder, sourceProvider)
     }
 
     public void registerMultiFlavorSourceProvider(

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/ExtraModelInfo.java
@@ -222,6 +222,7 @@ public class ExtraModelInfo {
             @NonNull Iterable<String> ideSetupTaskNames,
             @NonNull Configuration configuration,
             @NonNull File classesFolder,
+            @NonNull File javaResourcesFolder,
             @Nullable SourceProvider sourceProvider) {
         ArtifactMetaData artifactMetaData = extraArtifactMap.get(name);
         if (artifactMetaData == null) {
@@ -235,8 +236,9 @@ public class ExtraModelInfo {
 
         JavaArtifact artifact = new JavaArtifactImpl(
                 name, assembleTaskName, javaCompileTaskName, ideSetupTaskNames,
-                generatedSourceFolders, classesFolder, new ConfigurationDependencies(configuration),
-                sourceProvider, null);
+                generatedSourceFolders, classesFolder, javaResourcesFolder,
+                new ConfigurationDependencies(configuration), sourceProvider, null);
+
         extraJavaArtifacts.put(variant.getName(), artifact);
     }
 

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/model/AndroidArtifactImpl.java
@@ -69,14 +69,16 @@ public class AndroidArtifactImpl extends BaseArtifactImpl implements AndroidArti
             @NonNull List<File> generatedSourceFolders,
             @NonNull List<File> generatedResourceFolders,
             @NonNull File classesFolder,
+            @NonNull File javaResourcesFolder,
             @NonNull Dependencies dependencies,
             @Nullable SourceProvider variantSourceProvider,
             @Nullable SourceProvider multiFlavorSourceProviders,
             @Nullable Set<String> abiFilters,
             @NonNull Map<String,ClassField> buildConfigFields,
             @NonNull Map<String,ClassField> resValues) {
-        super(name, assembleTaskName, compileTaskName, classesFolder, dependencies,
-                variantSourceProvider, multiFlavorSourceProviders, generatedSourceFolders);
+        super(name, assembleTaskName, compileTaskName, classesFolder, javaResourcesFolder,
+                dependencies, variantSourceProvider, multiFlavorSourceProviders,
+                generatedSourceFolders);
 
         this.outputs = outputs;
         this.isSigned = isSigned;

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/model/JavaArtifactImpl.java
@@ -47,6 +47,7 @@ public class JavaArtifactImpl extends BaseArtifactImpl implements JavaArtifact, 
                 javaArtifact.getIdeSetupTaskNames(),
                 javaArtifact.getGeneratedSourceFolders(),
                 javaArtifact.getClassesFolder(),
+                javaArtifact.getJavaResourcesFolder(),
                 DependenciesImpl.cloneDependenciesForJavaArtifacts(javaArtifact.getDependencies()),
                 variantSP != null ? SourceProviderImpl.cloneProvider(variantSP) : null,
                 flavorsSP != null ? SourceProviderImpl.cloneProvider(flavorsSP) : null);
@@ -58,10 +59,12 @@ public class JavaArtifactImpl extends BaseArtifactImpl implements JavaArtifact, 
                             @NonNull Iterable<String> ideSetupTaskNames,
                             @NonNull Collection<File> generatedSourceFolders,
                             @NonNull File classesFolder,
+                            @NonNull File javaResourcesFolder,
                             @NonNull Dependencies dependencies,
                             @Nullable SourceProvider variantSourceProvider,
                             @Nullable SourceProvider multiFlavorSourceProviders) {
-        super(name, assembleTaskName, compileTaskName, classesFolder, dependencies,
+        super(name, assembleTaskName, compileTaskName, classesFolder, javaResourcesFolder,
+                dependencies,
                 variantSourceProvider, multiFlavorSourceProviders, generatedSourceFolders);
         this.ideSetupTaskNames = Sets.newHashSet(ideSetupTaskNames);
     }

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/UnitTestingModelTest.groovy
@@ -77,6 +77,11 @@ android {
             // No per-variant source code.
             assertThat(unitTestArtifact.variantSourceProvider).isNull()
             assertThat(unitTestArtifact.multiFlavorSourceProvider).isNull()
+
+            assertThat(variant.mainArtifact.javaResourcesFolder.path)
+                    .endsWith("intermediates/javaResources/" + variant.name)
+            assertThat(unitTestArtifact.javaResourcesFolder.path)
+                    .endsWith("intermediates/javaResources/test/" + variant.name)
         }
 
         def sourceProvider = model.defaultConfig