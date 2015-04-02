tadfisher/platform_tools_base

Add getRuntimeJavaResourcesFolder to BaseArtifact.
This way we can start putting it on the classpath when running unit
tests from AS.

Change-Id: I2abf0b61797e5f24c08d3640b66b76a87ed357b4

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/model/AndroidArtifactImpl.java
@@ -69,14 +69,16 @@ public class AndroidArtifactImpl extends BaseArtifactImpl implements AndroidArti
             @NonNull List<File> generatedSourceFolders,
             @NonNull List<File> generatedResourceFolders,
             @NonNull File classesFolder,
+            @NonNull File runtimeJavaResourcesFolder,
             @NonNull Dependencies dependencies,
             @Nullable SourceProvider variantSourceProvider,
             @Nullable SourceProvider multiFlavorSourceProviders,
             @Nullable Set<String> abiFilters,
             @NonNull Map<String,ClassField> buildConfigFields,
             @NonNull Map<String,ClassField> resValues) {
-        super(name, assembleTaskName, compileTaskName, classesFolder, dependencies,
-                variantSourceProvider, multiFlavorSourceProviders, generatedSourceFolders);
+        super(name, assembleTaskName, compileTaskName, classesFolder, runtimeJavaResourcesFolder,
+                dependencies, variantSourceProvider, multiFlavorSourceProviders,
+                generatedSourceFolders);
 
         this.outputs = outputs;
         this.isSigned = isSigned;

didot/idea-adt-tools-base-mirror

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/model/AndroidArtifactImpl.java
@@ -68,14 +68,16 @@ public class AndroidArtifactImpl extends BaseArtifactImpl implements AndroidArti
             @NonNull List<File> generatedSourceFolders,
             @NonNull List<File> generatedResourceFolders,
             @NonNull File classesFolder,
+            @NonNull File javaResourcesFolder,
             @NonNull Dependencies dependencies,
             @Nullable SourceProvider variantSourceProvider,
             @Nullable SourceProvider multiFlavorSourceProviders,
             @Nullable Set<String> abiFilters,
             @NonNull Map<String,ClassField> buildConfigFields,
             @NonNull Map<String,ClassField> resValues) {
-        super(name, assembleTaskName, compileTaskName, classesFolder, dependencies,
-                variantSourceProvider, multiFlavorSourceProviders, generatedSourceFolders);
+        super(name, assembleTaskName, compileTaskName, classesFolder, javaResourcesFolder,
+                dependencies, variantSourceProvider, multiFlavorSourceProviders,
+                generatedSourceFolders);
 
         this.outputs = outputs;
         this.isSigned = isSigned;