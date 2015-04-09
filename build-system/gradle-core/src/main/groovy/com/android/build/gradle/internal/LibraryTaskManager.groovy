tadfisher/platform_tools_base

allow separate test modules to test proguarded applications.
added the configuration dependencies on mapping and classes.jar
and use these configurations to determine if the proguard class really
need to run.

so far, I am not jaring up the classes in case proguard did not run
but rather changed the Dex task to be more flexible in accepting
either classes or jar files (to be discussed.)

Change-Id: Ib5c38ddf423584499c85b7a6ecbfb0b0aa8b9c2a

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/LibraryTaskManager.groovy
@@ -260,7 +260,10 @@ class LibraryTaskManager extends TaskManager {
         if (buildType.isMinifyEnabled()) {
             // run proguard on output of compile task
             SpanRecorders.record(ExecutionType.LIB_TASK_MANAGER_CREATE_PROGUARD_TASK) {
-                createProguardTasks(variantData, null, pcData)
+                File outFile = createProguardTasks(variantData, null, pcData)
+                pcData.inputFiles = { [outFile] }
+                pcData.inputDir = null
+                pcData.inputLibraries = { [] }
             }
         } else {
             // package the local jar in libs/

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/TaskManager.groovy
@@ -2081,14 +2081,14 @@ abstract class TaskManager {
         }
 
         // ----- Minify next ----
+        BaseVariantData<? extends BaseVariantOutputData> testedVariantData =
+                (variantData instanceof TestVariantData ? variantData.testedVariantData :
+                        null) as BaseVariantData
 
-        if (isMinifyEnabled) {
-            // first proguard task.
-            BaseVariantData<? extends BaseVariantOutputData> testedVariantData =
-                    (variantData instanceof TestVariantData ? variantData.testedVariantData :
-                            null) as BaseVariantData
-            createProguardTasks(variantData, testedVariantData, pcData)
-
+        File outFile = createProguardTasks(variantData, testedVariantData, pcData);
+        if (outFile != null) {
+            pcData.inputFiles = { [outFile] }
+            pcData.inputLibraries = { [] }
         } else if ((getExtension().dexOptions.preDexLibraries && !isMultiDexEnabled) ||
                 (isMultiDexEnabled && !isLegacyMultiDexMode)) {
             def preDexTaskName = "preDex${config.fullName.capitalize()}"
@@ -2247,15 +2247,15 @@ abstract class TaskManager {
         // ----- Dex Task ----
 
         // dependencies, some of these could be null
+        optionalDependsOn(dexTask, variantData.obfuscationTask)
         optionalDependsOn(dexTask, pcData.classGeneratingTask)
         optionalDependsOn(dexTask, pcData.libraryGeneratingTask,)
 
         // inputs
         if (pcData.inputDir != null) {
             conventionMapping(dexTask).map("inputDir", pcData.inputDir)
-        } else {
-            conventionMapping(dexTask).map("inputFiles", pcData.inputFiles)
         }
+        conventionMapping(dexTask).map("inputFiles", pcData.inputFiles)
         conventionMapping(dexTask).map("libraries", pcData.inputLibraries)
     }
 
@@ -2875,13 +2875,18 @@ abstract class TaskManager {
      * @param variantData the variant data.
      * @param testedVariantData optional. variant data representing the tested variant, null if the
      *                          variant is not a test variant
-     * @return outFile file outputted by proguard
+     * @return null if the proguard task was not created, otherwise the intented outputFile.
      */
-    @NonNull
-    public void createProguardTasks(
+    @Nullable
+    public File createProguardTasks(
             final @NonNull BaseVariantData<? extends BaseVariantOutputData> variantData,
             final @Nullable BaseVariantData<? extends BaseVariantOutputData> testedVariantData,
             final @NonNull PostCompilationData pcData) {
+
+        if (!variantData.getVariantConfiguration().isMinifyEnabled()) {
+            return null;
+        }
+
         final VariantConfiguration variantConfig = variantData.variantConfiguration
 
         // use single output for now.
@@ -3048,9 +3053,7 @@ abstract class TaskManager {
         pcData.classGeneratingTask = [proguardTask]
 
         // Update the inputs
-        pcData.inputFiles = { [outFile] }
-        pcData.inputDir = null
-        pcData.inputLibraries = { [] }
+        return outFile;
     }
 
     private ShrinkResources createShrinkResourcesTask(ApkVariantOutputData variantOutputData) {

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/TestApplicationTaskManager.java
@@ -19,23 +19,36 @@ package com.android.build.gradle.internal;
 import static com.android.builder.core.BuilderConstants.CONNECTED;
 
 import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
 import com.android.build.gradle.BaseExtension;
 import com.android.build.gradle.TestExtension;
 import com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask;
 import com.android.build.gradle.internal.test.TestApplicationTestData;
 import com.android.build.gradle.internal.variant.BaseVariantData;
 import com.android.build.gradle.internal.variant.BaseVariantOutputData;
+import com.android.build.gradle.internal.variant.LibraryVariantData;
+import com.android.build.gradle.tasks.TestModuleProGuardTask;
 import com.android.builder.core.AndroidBuilder;
+import com.android.builder.model.AndroidProject;
 import com.android.builder.testing.ConnectedDeviceProvider;
 import com.android.builder.testing.TestData;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
+import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry;
+
+import static com.android.builder.model.AndroidProject.FD_OUTPUTS;
+
+
+import java.io.File;
+import java.util.Locale;
+
+import proguard.ParseException;
 
 /**
  * TaskManager for standalone test application that lives in a separate module from the tested
@@ -97,11 +110,147 @@ public class TestApplicationTaskManager extends ApplicationTaskManager {
         // make the test application connectedCheck depends on the configuration added above so
         // we can retrieve its artifacts
         testConnectedCheck.dependsOn(testTarget);
+        testConnectedCheck.dependsOn(testTargetMetadata);
 
         // make the main ConnectedCheck task depends on this test connectedCheck
         Task connectedCheck = tasks.named(CONNECTED_CHECK);
         if (connectedCheck != null) {
             connectedCheck.dependsOn(testConnectedCheck);
+        }
+    }
+
+    @Override
+    @Nullable
+    public File createProguardTasks(
+            @NonNull BaseVariantData<? extends BaseVariantOutputData> variantData,
+            @Nullable BaseVariantData<? extends BaseVariantOutputData> testedVariantData,
+            @NonNull PostCompilationData pcData) {
+
+        final TestModuleProGuardTask proguardTask = project.getTasks().create(
+                "proguard"+ variantData.getVariantConfiguration().getFullName().toUpperCase(
+                        Locale.getDefault()),
+                TestModuleProGuardTask.class);
+
+        variantData.obfuscationTask = proguardTask;
+
+        // --- Output File ---
+
+        final File outFile = variantData instanceof LibraryVariantData ?
+                project.file(String.format("%s/%s/%s/%s/classes.jar",
+                        project.getBuildDir(),
+                        AndroidProject.FD_INTERMEDIATES,
+                        TaskManager.DIR_BUNDLES,
+                        variantData.getVariantConfiguration().getDirName())) :
+                project.file(String.format("%s/%s/classes-proguard/%s/classes.jar",
+                        project.getBuildDir(),
+                        AndroidProject.FD_INTERMEDIATES,
+                        variantData.getVariantConfiguration().getDirName()));
+        variantData.obfuscatedClassesJar = outFile;
+
+        DependencyHandler dependencyHandler = project.getDependencies();
+        TestExtension testExtension = (TestExtension) extension;
+
+        // and create the configuration for the project's classes.jar file.
+        Configuration testClassesMapping = project.getConfigurations().create("testTargetClasses");
+
+        dependencyHandler.add("testTargetClasses", dependencyHandler.project(
+                ImmutableMap.of(
+                        "path", testExtension.getTargetProjectPath(),
+                        "configuration", testExtension.getTargetVariant() + "-classes"
+                )));
+
+        proguardTask.dependsOn(testClassesMapping);
+
+        // and create the configuration for the project's mapping file.
+        Configuration testTargetMapping = project.getConfigurations().create("testTargetMapping");
+
+        dependencyHandler.add("testTargetMapping", dependencyHandler.project(
+                ImmutableMap.of(
+                        "path", testExtension.getTargetProjectPath(),
+                        "configuration", testExtension.getTargetVariant() + "-mapping"
+                )));
+
+        proguardTask.dependsOn(testTargetMapping);
+
+        // --- Proguard Config ---
+
+        // Don't remove any code in tested app.
+        proguardTask.dontshrink();
+        proguardTask.dontoptimize();
+
+        // We can't call dontobfuscate, since that would make ProGuard ignore the mapping file.
+        try {
+            proguardTask.keep("class * {*;}");
+            proguardTask.keep("interface * {*;}");
+            proguardTask.keep("enum * {*;}");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Input the mapping from the tested app so that we can deal with obfuscated code.
+        proguardTask.setMappingConfiguration(testTargetMapping);
+        proguardTask.setClassesConfiguration(testClassesMapping);
+
+        // libraryJars: the runtime jars. Do this in doFirst since the boot classpath isn't
+        // available until the SDK is loaded in the prebuild task
+        proguardTask.doFirst(new Action<Task>() {
+            @Override
+            public void execute(Task task) {
+                for (String runtimeJar : androidBuilder.getBootClasspathAsStrings()) {
+                    try {
+                        proguardTask.libraryjars(runtimeJar);
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        try {
+            // injar: the compilation output
+            proguardTask.injars(pcData.getInputDir());
+
+            // All -dontwarn rules for test dependencies should go in here:
+            proguardTask.configuration(
+                    variantData.getVariantConfiguration().getTestProguardFiles());
+
+
+            // --- Out files ---
+
+            proguardTask.outjars(outFile);
+
+            final File proguardOut = project.file(
+                    new File(project.getBuildDir(),
+                            FD_OUTPUTS
+                                    + File.separatorChar
+                                    + "mapping"
+                                    + File.separatorChar
+                                    + variantData.getVariantConfiguration().getDirName()));
+
+            proguardTask.dump(new File(proguardOut, "dump.txt"));
+            proguardTask.printseeds(new File(proguardOut, "seeds.txt"));
+            proguardTask.printusage(new File(proguardOut, "usage.txt"));
+            proguardTask.printmapping(new File(proguardOut, "mapping.txt"));
+
+            // proguard doesn't verify that the seed/mapping/usage folders exist and will fail
+            // if they don't so create them.
+            proguardTask.doFirst(new Action<Task>() {
+                @Override
+                public void execute(Task task) {
+                    proguardOut.mkdirs();
+                }
+            });
+
+            // update dependency.
+            optionalDependsOn(proguardTask, pcData.getClassGeneratingTask());
+            optionalDependsOn(proguardTask, pcData.getLibraryGeneratingTask());
+            pcData.setLibraryGeneratingTask(ImmutableList.of(proguardTask));
+            pcData.setClassGeneratingTask(ImmutableList.of(proguardTask));
+
+            // Update the inputs
+            return outFile;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }

didot/idea-adt-tools-base-mirror

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/LibraryTaskManager.groovy
@@ -260,7 +260,10 @@ class LibraryTaskManager extends TaskManager {
         if (buildType.isMinifyEnabled()) {
             // run proguard on output of compile task
             SpanRecorders.record(ExecutionType.LIB_TASK_MANAGER_CREATE_PROGUARD_TASK) {
-                createProguardTasks(variantData, null, pcData)
+                File outFile = maybeCreateProguardTasks(variantData, null, pcData)
+                pcData.inputFiles = { [outFile] }
+                pcData.inputDir = null
+                pcData.inputLibraries = { [] }
             }
         } else {
             // package the local jar in libs/

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/TaskManager.groovy
@@ -2081,14 +2081,14 @@ abstract class TaskManager {
         }
 
         // ----- Minify next ----
+        BaseVariantData<? extends BaseVariantOutputData> testedVariantData =
+                (variantData instanceof TestVariantData ? variantData.testedVariantData :
+                        null) as BaseVariantData
 
-        if (isMinifyEnabled) {
-            // first proguard task.
-            BaseVariantData<? extends BaseVariantOutputData> testedVariantData =
-                    (variantData instanceof TestVariantData ? variantData.testedVariantData :
-                            null) as BaseVariantData
-            createProguardTasks(variantData, testedVariantData, pcData)
-
+        File outFile = maybeCreateProguardTasks(variantData, testedVariantData, pcData);
+        if (outFile != null) {
+            pcData.inputFiles = { [outFile] }
+            pcData.inputLibraries = { [] }
         } else if ((getExtension().dexOptions.preDexLibraries && !isMultiDexEnabled) ||
                 (isMultiDexEnabled && !isLegacyMultiDexMode)) {
             def preDexTaskName = "preDex${config.fullName.capitalize()}"
@@ -2247,15 +2247,15 @@ abstract class TaskManager {
         // ----- Dex Task ----
 
         // dependencies, some of these could be null
+        optionalDependsOn(dexTask, variantData.obfuscationTask)
         optionalDependsOn(dexTask, pcData.classGeneratingTask)
         optionalDependsOn(dexTask, pcData.libraryGeneratingTask,)
 
         // inputs
         if (pcData.inputDir != null) {
             conventionMapping(dexTask).map("inputDir", pcData.inputDir)
-        } else {
-            conventionMapping(dexTask).map("inputFiles", pcData.inputFiles)
         }
+        conventionMapping(dexTask).map("inputFiles", pcData.inputFiles)
         conventionMapping(dexTask).map("libraries", pcData.inputLibraries)
     }
 
@@ -2871,17 +2871,22 @@ abstract class TaskManager {
     }
 
     /**
-     * Creates the proguarding task for the given Variant.
+     * Creates the proguarding task for the given Variant if necessary.
      * @param variantData the variant data.
      * @param testedVariantData optional. variant data representing the tested variant, null if the
      *                          variant is not a test variant
-     * @return outFile file outputted by proguard
+     * @return null if the proguard task was not created, otherwise the expected outputFile.
      */
-    @NonNull
-    public void createProguardTasks(
+    @Nullable
+    public File maybeCreateProguardTasks(
             final @NonNull BaseVariantData<? extends BaseVariantOutputData> variantData,
             final @Nullable BaseVariantData<? extends BaseVariantOutputData> testedVariantData,
             final @NonNull PostCompilationData pcData) {
+
+        if (!variantData.getVariantConfiguration().isMinifyEnabled()) {
+            return null;
+        }
+
         final VariantConfiguration variantConfig = variantData.variantConfiguration
 
         // use single output for now.
@@ -3048,9 +3053,7 @@ abstract class TaskManager {
         pcData.classGeneratingTask = [proguardTask]
 
         // Update the inputs
-        pcData.inputFiles = { [outFile] }
-        pcData.inputDir = null
-        pcData.inputLibraries = { [] }
+        return outFile;
     }
 
     private ShrinkResources createShrinkResourcesTask(ApkVariantOutputData variantOutputData) {

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/TestApplicationTaskManager.java
@@ -19,23 +19,36 @@ package com.android.build.gradle.internal;
 import static com.android.builder.core.BuilderConstants.CONNECTED;
 
 import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
 import com.android.build.gradle.BaseExtension;
 import com.android.build.gradle.TestExtension;
 import com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask;
 import com.android.build.gradle.internal.test.TestApplicationTestData;
 import com.android.build.gradle.internal.variant.BaseVariantData;
 import com.android.build.gradle.internal.variant.BaseVariantOutputData;
+import com.android.build.gradle.internal.variant.LibraryVariantData;
+import com.android.build.gradle.tasks.TestModuleProGuardTask;
 import com.android.builder.core.AndroidBuilder;
+import com.android.builder.model.AndroidProject;
 import com.android.builder.testing.ConnectedDeviceProvider;
 import com.android.builder.testing.TestData;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
+import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry;
+
+import static com.android.builder.model.AndroidProject.FD_OUTPUTS;
+
+
+import java.io.File;
+import java.util.Locale;
+
+import proguard.ParseException;
 
 /**
  * TaskManager for standalone test application that lives in a separate module from the tested
@@ -97,11 +110,145 @@ public class TestApplicationTaskManager extends ApplicationTaskManager {
         // make the test application connectedCheck depends on the configuration added above so
         // we can retrieve its artifacts
         testConnectedCheck.dependsOn(testTarget);
+        testConnectedCheck.dependsOn(testTargetMetadata);
 
         // make the main ConnectedCheck task depends on this test connectedCheck
         Task connectedCheck = tasks.named(CONNECTED_CHECK);
         if (connectedCheck != null) {
             connectedCheck.dependsOn(testConnectedCheck);
+        }
+    }
+
+    @Override
+    @Nullable
+    public File maybeCreateProguardTasks(
+            @NonNull BaseVariantData<? extends BaseVariantOutputData> variantData,
+            @Nullable BaseVariantData<? extends BaseVariantOutputData> testedVariantData,
+            @NonNull PostCompilationData pcData) {
+
+        final TestModuleProGuardTask proguardTask = project.getTasks().create(
+                "proguard"+ variantData.getVariantConfiguration().getFullName().toUpperCase(
+                        Locale.getDefault()),
+                TestModuleProGuardTask.class);
+
+        variantData.obfuscationTask = proguardTask;
+
+        // --- Output File ---
+
+        final File outFile = variantData instanceof LibraryVariantData ?
+                project.file(String.format("%s/%s/%s/%s/classes.jar",
+                        project.getBuildDir(),
+                        AndroidProject.FD_INTERMEDIATES,
+                        TaskManager.DIR_BUNDLES,
+                        variantData.getVariantConfiguration().getDirName())) :
+                project.file(String.format("%s/%s/classes-proguard/%s/classes.jar",
+                        project.getBuildDir(),
+                        AndroidProject.FD_INTERMEDIATES,
+                        variantData.getVariantConfiguration().getDirName()));
+        variantData.obfuscatedClassesJar = outFile;
+
+        DependencyHandler dependencyHandler = project.getDependencies();
+        TestExtension testExtension = (TestExtension) extension;
+
+        // and create the configuration for the project's classes.jar file.
+        Configuration testClassesMapping = project.getConfigurations().create("testTargetClasses");
+
+        dependencyHandler.add("testTargetClasses", dependencyHandler.project(
+                ImmutableMap.of(
+                        "path", testExtension.getTargetProjectPath(),
+                        "configuration", testExtension.getTargetVariant() + "-classes"
+                )));
+
+        // Input the original .class files so the compiler can compile the test code correctly.
+        proguardTask.setClassesConfiguration(testClassesMapping);
+
+        // and create the configuration for the project's mapping file.
+        Configuration testTargetMapping = project.getConfigurations().create("testTargetMapping");
+
+        dependencyHandler.add("testTargetMapping", dependencyHandler.project(
+                ImmutableMap.of(
+                        "path", testExtension.getTargetProjectPath(),
+                        "configuration", testExtension.getTargetVariant() + "-mapping"
+                )));
+
+        // Input the mapping from the tested app so that we can deal with obfuscated code.
+        proguardTask.setMappingConfiguration(testTargetMapping);
+
+        // --- Proguard Config ---
+
+        // Don't remove any code in tested app.
+        proguardTask.dontshrink();
+        proguardTask.dontoptimize();
+
+        // We can't call dontobfuscate, since that would make ProGuard ignore the mapping file.
+        try {
+            proguardTask.keep("class * {*;}");
+            proguardTask.keep("interface * {*;}");
+            proguardTask.keep("enum * {*;}");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        // libraryJars: the runtime jars. Do this in doFirst since the boot classpath isn't
+        // available until the SDK is loaded in the prebuild task
+        proguardTask.doFirst(new Action<Task>() {
+            @Override
+            public void execute(Task task) {
+                for (String runtimeJar : androidBuilder.getBootClasspathAsStrings()) {
+                    try {
+                        proguardTask.libraryjars(runtimeJar);
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        try {
+            // injar: the compilation output
+            proguardTask.injars(pcData.getInputDir());
+
+            // All -dontwarn rules for test dependencies should go in here:
+            proguardTask.configuration(
+                    variantData.getVariantConfiguration().getTestProguardFiles());
+
+
+            // --- Out files ---
+
+            proguardTask.outjars(outFile);
+
+            final File proguardOut = project.file(
+                    new File(project.getBuildDir(),
+                            FD_OUTPUTS
+                                    + File.separatorChar
+                                    + "mapping"
+                                    + File.separatorChar
+                                    + variantData.getVariantConfiguration().getDirName()));
+
+            proguardTask.dump(new File(proguardOut, "dump.txt"));
+            proguardTask.printseeds(new File(proguardOut, "seeds.txt"));
+            proguardTask.printusage(new File(proguardOut, "usage.txt"));
+            proguardTask.printmapping(new File(proguardOut, "mapping.txt"));
+
+            // proguard doesn't verify that the seed/mapping/usage folders exist and will fail
+            // if they don't so create them.
+            proguardTask.doFirst(new Action<Task>() {
+                @Override
+                public void execute(Task task) {
+                    proguardOut.mkdirs();
+                }
+            });
+
+            // update dependency.
+            optionalDependsOn(proguardTask, pcData.getClassGeneratingTask());
+            optionalDependsOn(proguardTask, pcData.getLibraryGeneratingTask());
+            pcData.setLibraryGeneratingTask(ImmutableList.of(proguardTask));
+            pcData.setClassGeneratingTask(ImmutableList.of(proguardTask));
+
+            // Update the inputs
+            return outFile;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }