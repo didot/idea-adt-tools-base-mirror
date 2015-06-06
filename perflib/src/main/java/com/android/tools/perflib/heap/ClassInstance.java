tadfisher/platform_tools_base

Dominator computation now always excludes weak references.
Dominator computation now excludes weak/soft/phantom/finalized
references in its dominator computation.

Weak references are also now taken care of in the shortest distance
visitor for depth computation. When a link whose parent is a weak
reference and the child is a non-weak reference, the instance will not
be added to the frontier computation for depth purposes.

Added various refactoring for better naming.

Change-Id: I57016ce346738c63aaf0efdbed54071c11d00756

perflib/src/main/java/com/android/tools/perflib/heap/ClassInstance.java
@@ -59,10 +59,15 @@ public class ClassInstance extends Instance {
                 if (!mReferencesAdded) {
                     ((Instance)value).addReference(this);
                 }
-                visitor.visitLater((Instance) value);
+                visitor.visitLater(this, (Instance)value);
             }
         }
         mReferencesAdded = true;
+    }
+
+    @Override
+    public boolean getIsSoftReferenceType() {
+        return getClassObj().getIsSoftReferenceType();
     }
 
     public final String toString() {

perflib/src/main/java/com/android/tools/perflib/heap/analysis/ShortestDistanceVisitor.java
@@ -18,7 +18,6 @@ package com.android.tools.perflib.heap.analysis;
 import com.android.annotations.NonNull;
 import com.android.tools.perflib.heap.Instance;
 import com.android.tools.perflib.heap.NonRecursiveVisitor;
-import gnu.trove.TLongHashSet;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
@@ -30,17 +29,16 @@ public class ShortestDistanceVisitor extends NonRecursiveVisitor {
             return o1.getDistanceToGcRoot() - o2.getDistanceToGcRoot();
         }
     });
-    private TLongHashSet mExistsInQueue = new TLongHashSet();
     private Instance mPreviousInstance = null;
     private int mVisitDistance = 0;
 
     @Override
-    public void visitLater(@NonNull Instance instance) {
-        if (mVisitDistance < instance.getDistanceToGcRoot()) {
-            instance.setDistanceToGcRoot(mVisitDistance);
-            instance.setNextInstanceToGcRoot(mPreviousInstance);
-            mExistsInQueue.add(instance.getId());
-            mPriorityQueue.add(instance);
+    public void visitLater(Instance parent, @NonNull Instance child) {
+        if (mVisitDistance < child.getDistanceToGcRoot() &&
+                (parent == null || !parent.getIsSoftReferenceType() || child.getIsSoftReferenceType())) {
+            child.setDistanceToGcRoot(mVisitDistance);
+            child.setNextInstanceToGcRoot(mPreviousInstance);
+            mPriorityQueue.add(child);
         }
     }
 
@@ -56,10 +54,9 @@ public class ShortestDistanceVisitor extends NonRecursiveVisitor {
 
         while (!mPriorityQueue.isEmpty()) {
             Instance node = mPriorityQueue.poll();
-            mExistsInQueue.remove(node.getId());
             mVisitDistance = node.getDistanceToGcRoot() + 1;
             mPreviousInstance = node;
             node.accept(this);
-          }
+        }
     }
 }

didot/idea-adt-tools-base-mirror

perflib/src/main/java/com/android/tools/perflib/heap/ClassInstance.java
@@ -59,10 +59,15 @@ public class ClassInstance extends Instance {
                 if (!mReferencesAdded) {
                     ((Instance)value).addReference(this);
                 }
-                visitor.visitLater((Instance) value);
+                visitor.visitLater(this, (Instance)value);
             }
         }
         mReferencesAdded = true;
+    }
+
+    @Override
+    public boolean getIsSoftReference() {
+        return getClassObj().getIsSoftReference();
     }
 
     public final String toString() {

perflib/src/main/java/com/android/tools/perflib/heap/analysis/ShortestDistanceVisitor.java
@@ -18,7 +18,6 @@ package com.android.tools.perflib.heap.analysis;
 import com.android.annotations.NonNull;
 import com.android.tools.perflib.heap.Instance;
 import com.android.tools.perflib.heap.NonRecursiveVisitor;
-import gnu.trove.TLongHashSet;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
@@ -30,17 +29,16 @@ public class ShortestDistanceVisitor extends NonRecursiveVisitor {
             return o1.getDistanceToGcRoot() - o2.getDistanceToGcRoot();
         }
     });
-    private TLongHashSet mExistsInQueue = new TLongHashSet();
     private Instance mPreviousInstance = null;
     private int mVisitDistance = 0;
 
     @Override
-    public void visitLater(@NonNull Instance instance) {
-        if (mVisitDistance < instance.getDistanceToGcRoot()) {
-            instance.setDistanceToGcRoot(mVisitDistance);
-            instance.setNextInstanceToGcRoot(mPreviousInstance);
-            mExistsInQueue.add(instance.getId());
-            mPriorityQueue.add(instance);
+    public void visitLater(Instance parent, @NonNull Instance child) {
+        if (mVisitDistance < child.getDistanceToGcRoot() &&
+                (parent == null || !parent.getIsSoftReference() || child.getIsSoftReference())) {
+            child.setDistanceToGcRoot(mVisitDistance);
+            child.setNextInstanceToGcRoot(mPreviousInstance);
+            mPriorityQueue.add(child);
         }
     }
 
@@ -56,10 +54,9 @@ public class ShortestDistanceVisitor extends NonRecursiveVisitor {
 
         while (!mPriorityQueue.isEmpty()) {
             Instance node = mPriorityQueue.poll();
-            mExistsInQueue.remove(node.getId());
             mVisitDistance = node.getDistanceToGcRoot() + 1;
             mPreviousInstance = node;
             node.accept(this);
-          }
+        }
     }
 }