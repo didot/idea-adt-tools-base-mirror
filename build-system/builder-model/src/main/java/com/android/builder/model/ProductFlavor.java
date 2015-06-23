tadfisher/platform_tools_base

Support variant specific NDK options.
NDK options such as C flags specified in BuildTypes and ProductFlavors are now
merged and used to configure the native binaries.

ProductFlavorCombo is modified to support managed.ProductFlavor, which does
not inherit from model.ProductFlavor.  A Groupable interface is created to
support this.

getNdkConfig is rename to getNdk to simplify DSL.

core.NdkConfig is renamed to CoreNdkOptions for consistency.

Change-Id: Ifea600b6b7e6ab33fb1917f4f266ff6fb98be474

build-system/builder-model/src/main/java/com/android/builder/model/ProductFlavor.java
@@ -31,7 +31,7 @@ import java.util.Map;
  * @see ProductFlavorContainer
  * @see BaseArtifact#getDependencies()
  */
-public interface ProductFlavor extends BaseConfig {
+public interface ProductFlavor extends BaseConfig, Groupable {
 
     /**
      * Returns the name of the flavor.
@@ -41,12 +41,6 @@ public interface ProductFlavor extends BaseConfig {
     @Override
     @NonNull
     String getName();
-
-    /**
-     * Returns the flavor dimension or null if not applicable.
-     */
-    @Nullable
-    String getDimension();
 
     /**
      * Returns the name of the product flavor. This is only the value set on this product flavor.

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/ProductFlavorCombo.java
@@ -18,33 +18,39 @@ package com.android.build.gradle.internal;
 
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
+import com.android.builder.model.Groupable;
 import com.android.builder.model.ProductFlavor;
 import com.android.utils.StringHelper;
+import com.google.common.base.Predicates;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Lists;
 
+import org.gradle.api.Named;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * A combination of product flavors for a variant, each belonging to a different flavor dimension.
  */
-public class ProductFlavorCombo {
+public class ProductFlavorCombo<T extends Groupable & Named> {
     private String name;
 
     @NonNull
-    private final List<ProductFlavor> flavorList;
+    private final List<T> flavorList;
 
     /**
      * Create a ProductFlavorCombo.
      * @param flavors Lists of ProductFlavor.
      */
-    public ProductFlavorCombo(@NonNull ProductFlavor... flavors) {
+    public ProductFlavorCombo(@NonNull T... flavors) {
         flavorList = ImmutableList.copyOf(flavors);
     }
 
-    public ProductFlavorCombo(@NonNull Iterable<? extends ProductFlavor> flavors) {
+    public ProductFlavorCombo(@NonNull Iterable<T> flavors) {
         flavorList = ImmutableList.copyOf(flavors);
     }
 
@@ -53,7 +59,7 @@ public class ProductFlavorCombo {
         if (name == null) {
             boolean first = true;
             StringBuilder sb = new StringBuilder();
-            for (ProductFlavor flavor : flavorList) {
+            for (T flavor : flavorList) {
                 if (first) {
                     sb.append(flavor.getName());
                     first = false;
@@ -67,7 +73,7 @@ public class ProductFlavorCombo {
     }
 
     @NonNull
-    public List<ProductFlavor> getFlavorList() {
+    public List<T> getFlavorList() {
         return flavorList;
     }
 
@@ -78,20 +84,20 @@ public class ProductFlavorCombo {
      * @return A list of ProductFlavorCombo representing all combinations of ProductFlavors.
      */
     @NonNull
-    public static List<ProductFlavorCombo> createCombinations(
+    public static <S extends Groupable & Named> List<ProductFlavorCombo<S>> createCombinations(
             @Nullable List<String> flavorDimensions,
-            @NonNull Iterable<? extends ProductFlavor> productFlavors) {
+            @NonNull Iterable<S> productFlavors) {
 
-        List <ProductFlavorCombo> result = Lists.newArrayList();
+        List <ProductFlavorCombo<S>> result = Lists.newArrayList();
         if (flavorDimensions == null || flavorDimensions.isEmpty()) {
-            for (ProductFlavor flavor : productFlavors) {
-                result.add(new ProductFlavorCombo(ImmutableList.of(flavor)));
+            for (S flavor : productFlavors) {
+                result.add(new ProductFlavorCombo<S>(ImmutableList.of(flavor)));
             }
         } else {
             // need to group the flavor per dimension.
             // First a map of dimension -> list(ProductFlavor)
-            ArrayListMultimap<String, ProductFlavor> map = ArrayListMultimap.create();
-            for (ProductFlavor flavor : productFlavors) {
+            ArrayListMultimap<String, S> map = ArrayListMultimap.create();
+            for (S flavor : productFlavors) {
                 String flavorDimension = flavor.getDimension();
 
                 if (flavorDimension == null) {
@@ -108,7 +114,7 @@ public class ProductFlavorCombo {
             }
 
             createProductFlavorCombinations(result,
-                    new ProductFlavor[flavorDimensions.size()],
+                    Lists.<S>newArrayListWithCapacity(flavorDimensions.size()),
                     0, flavorDimensions, map);
         }
         return result;
@@ -127,14 +133,14 @@ public class ProductFlavorCombo {
         return builder.build();
     }
 
-    private static void createProductFlavorCombinations(
-            List<ProductFlavorCombo> flavorGroups,
-            ProductFlavor[] group,
+    private static <S extends Groupable & Named> void createProductFlavorCombinations(
+            List<ProductFlavorCombo<S>> flavorGroups,
+            List<S> group,
             int index,
             List<String> flavorDimensionList,
-            ListMultimap<String, ProductFlavor> map) {
+            ListMultimap<String, S> map) {
         if (index == flavorDimensionList.size()) {
-            flavorGroups.add(new ProductFlavorCombo(filterNullFromArray(group)));
+            flavorGroups.add(new ProductFlavorCombo<S>(Iterables.filter(group, Predicates.notNull())));
             return;
         }
 
@@ -143,7 +149,7 @@ public class ProductFlavorCombo {
         String dimension = flavorDimensionList.get(index);
 
         // from our map, get all the possible flavors in that dimension.
-        List<ProductFlavor> flavorList = map.get(dimension);
+        List<S> flavorList = map.get(dimension);
 
         // loop on all the flavors to add them to the current index and recursively fill the next
         // indices.
@@ -151,10 +157,11 @@ public class ProductFlavorCombo {
             throw new RuntimeException(String.format(
                     "No flavor is associated with flavor dimension '%1$s'.", dimension));
         } else {
-            for (ProductFlavor flavor : flavorList) {
-                group[index] = flavor;
+            for (S flavor : flavorList) {
+                group.add(flavor);
                 createProductFlavorCombinations(
                         flavorGroups, group, index + 1, flavorDimensionList, map);
+                group.remove(group.size() - 1);
             }
         }
     }

build-system/gradle-experimental/src/main/groovy/com/android/build/gradle/managed/ProductFlavor.java
@@ -20,6 +20,7 @@ import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.builder.model.AndroidArtifact;
 import com.android.builder.model.AndroidProject;
+import com.android.builder.model.Groupable;
 import com.android.builder.model.Variant;
 
 import org.gradle.api.Named;
@@ -41,7 +42,7 @@ import java.util.Map;
  * TODO: Convert Unmanaged Collection to Managed type when Gradle provides ModelSet for basic class.
  */
 @Managed
-public interface ProductFlavor extends Named {
+public interface ProductFlavor extends Named, Groupable {
 
     /**
      * Map of Build Config Fields where the key is the field name.
@@ -132,6 +133,7 @@ public interface ProductFlavor extends Named {
     /**
      * Returns the flavor dimension or null if not applicable.
      */
+    @Override
     @Nullable
     String getDimension();
     void setDimension(String dimension);
@@ -282,5 +284,5 @@ public interface ProductFlavor extends Named {
     Boolean getUseJack();
     void setUseJack(Boolean useJack);
 
-    NdkConfig getNdkConfig();
+    NdkOptions getNdk();
 }

build-system/gradle-experimental/src/main/groovy/com/android/build/gradle/ndk/internal/NdkConfiguration.java
@@ -77,15 +77,6 @@ public class NdkConfiguration {
                         getCCompiler(binary).define("ANDROID_NDK");
                         getCppCompiler(binary).define("ANDROID_NDK");
 
-                        // Set output library filename.
-                        binary.setSharedLibraryFile(
-                                new File(
-                                        buildDir,
-                                        NdkNamingScheme.getOutputDirectoryName(binary)
-                                                + "/"
-                                                + NdkNamingScheme.getSharedLibraryFileName(
-                                                        ndkConfig.getModuleName())));
-
                         // Replace output directory of compile tasks.
                         binary.getTasks().withType(CCompile.class, new Action<CCompile>() {
                             @Override
@@ -118,40 +109,62 @@ public class NdkConfiguration {
                         binary.getLinker().args("--sysroot=" + sysroot);
                         binary.getLinker().args("-Wl,--build-id");
 
-                        if (ndkConfig.getRenderscriptNdkMode()) {
-                            getCCompiler(binary).args("-I" + sysroot + "/usr/include/rs");
-                            getCCompiler(binary).args("-I" + sysroot + "/usr/include/rs/cpp");
-                            getCppCompiler(binary).args("-I" + sysroot + "/usr/include/rs");
-                            getCppCompiler(binary).args("-I" + sysroot + "/usr/include/rs/cpp");
-                            binary.getLinker().args("-L" + sysroot + "/usr/lib/rs");
-                        }
-
-                        NativeToolSpecificationFactory.create(
-                                ndkHandler,
-                                binary.getBuildType(),
-                                binary.getTargetPlatform()).apply(binary);
-
-                        // Add flags defined in NdkConfig
-                        for (String flag : ndkConfig.getCFlags()) {
-                            getCCompiler(binary).args(flag);
-                        }
-
-                        for (String flag : ndkConfig.getCppFlags()) {
-                            getCppCompiler(binary).args(flag);
-                        }
-
-                        for (String ldLib : ndkConfig.getLdLibs()) {
-                            binary.getLinker().args("-l" + ldLib);
-                        }
-
-                        StlNativeToolSpecification stlConfig = new StlNativeToolSpecification(
-                                ndkHandler,
-                                ndkConfig.getStl(),
-                                binary.getTargetPlatform());
-                        stlConfig.apply(binary);
                     }
 
                 });
+    }
+
+    /**
+     * Configure native binary with variant specific options.
+     */
+    public static void configureBinary(
+            SharedLibraryBinarySpec binary,
+            final File buildDir,
+            final NdkConfig ndkConfig,
+            final NdkHandler ndkHandler) {
+        // Set output library filename.
+        binary.setSharedLibraryFile(
+                new File(
+                        buildDir,
+                        NdkNamingScheme.getOutputDirectoryName(binary)
+                                + "/"
+                                + NdkNamingScheme.getSharedLibraryFileName(
+                                ndkConfig.getModuleName())));
+
+        String sysroot = ndkHandler.getSysroot(
+                Abi.getByName(binary.getTargetPlatform().getName()));
+
+        if (ndkConfig.getRenderscriptNdkMode()) {
+            getCCompiler(binary).args("-I" + sysroot + "/usr/include/rs");
+            getCCompiler(binary).args("-I" + sysroot + "/usr/include/rs/cpp");
+            getCppCompiler(binary).args("-I" + sysroot + "/usr/include/rs");
+            getCppCompiler(binary).args("-I" + sysroot + "/usr/include/rs/cpp");
+            binary.getLinker().args("-L" + sysroot + "/usr/lib/rs");
+        }
+
+        NativeToolSpecificationFactory.create(
+                ndkHandler,
+                binary.getBuildType(),
+                binary.getTargetPlatform()).apply(binary);
+
+        // Add flags defined in NdkConfig
+        for (String flag : ndkConfig.getCFlags()) {
+            getCCompiler(binary).args(flag);
+        }
+
+        for (String flag : ndkConfig.getCppFlags()) {
+            getCppCompiler(binary).args(flag);
+        }
+
+        for (String ldLib : ndkConfig.getLdLibs()) {
+            binary.getLinker().args("-l" + ldLib);
+        }
+
+        StlNativeToolSpecification stlConfig = new StlNativeToolSpecification(
+                ndkHandler,
+                ndkConfig.getStl(),
+                binary.getTargetPlatform());
+        stlConfig.apply(binary);
     }
 
     public static void createTasks(ModelMap<Task> tasks, SharedLibraryBinarySpec binary,

didot/idea-adt-tools-base-mirror

build-system/builder-model/src/main/java/com/android/builder/model/ProductFlavor.java
@@ -31,7 +31,7 @@ import java.util.Map;
  * @see ProductFlavorContainer
  * @see BaseArtifact#getDependencies()
  */
-public interface ProductFlavor extends BaseConfig {
+public interface ProductFlavor extends BaseConfig, DimensionAware {
 
     /**
      * Returns the name of the flavor.
@@ -41,12 +41,6 @@ public interface ProductFlavor extends BaseConfig {
     @Override
     @NonNull
     String getName();
-
-    /**
-     * Returns the flavor dimension or null if not applicable.
-     */
-    @Nullable
-    String getDimension();
 
     /**
      * Returns the name of the product flavor. This is only the value set on this product flavor.

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/ProductFlavorCombo.java
@@ -18,33 +18,38 @@ package com.android.build.gradle.internal;
 
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
+import com.android.builder.model.DimensionAware;
 import com.android.builder.model.ProductFlavor;
 import com.android.utils.StringHelper;
+import com.google.common.base.Predicates;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Lists;
+
+import org.gradle.api.Named;
 
 import java.util.List;
 
 /**
  * A combination of product flavors for a variant, each belonging to a different flavor dimension.
  */
-public class ProductFlavorCombo {
+public class ProductFlavorCombo<T extends DimensionAware & Named> {
     private String name;
 
     @NonNull
-    private final List<ProductFlavor> flavorList;
+    private final List<T> flavorList;
 
     /**
      * Create a ProductFlavorCombo.
      * @param flavors Lists of ProductFlavor.
      */
-    public ProductFlavorCombo(@NonNull ProductFlavor... flavors) {
+    public ProductFlavorCombo(@NonNull T... flavors) {
         flavorList = ImmutableList.copyOf(flavors);
     }
 
-    public ProductFlavorCombo(@NonNull Iterable<? extends ProductFlavor> flavors) {
+    public ProductFlavorCombo(@NonNull Iterable<T> flavors) {
         flavorList = ImmutableList.copyOf(flavors);
     }
 
@@ -53,7 +58,7 @@ public class ProductFlavorCombo {
         if (name == null) {
             boolean first = true;
             StringBuilder sb = new StringBuilder();
-            for (ProductFlavor flavor : flavorList) {
+            for (T flavor : flavorList) {
                 if (first) {
                     sb.append(flavor.getName());
                     first = false;
@@ -67,7 +72,7 @@ public class ProductFlavorCombo {
     }
 
     @NonNull
-    public List<ProductFlavor> getFlavorList() {
+    public List<T> getFlavorList() {
         return flavorList;
     }
 
@@ -78,20 +83,20 @@ public class ProductFlavorCombo {
      * @return A list of ProductFlavorCombo representing all combinations of ProductFlavors.
      */
     @NonNull
-    public static List<ProductFlavorCombo> createCombinations(
+    public static <S extends DimensionAware & Named> List<ProductFlavorCombo<S>> createCombinations(
             @Nullable List<String> flavorDimensions,
-            @NonNull Iterable<? extends ProductFlavor> productFlavors) {
+            @NonNull Iterable<S> productFlavors) {
 
-        List <ProductFlavorCombo> result = Lists.newArrayList();
+        List <ProductFlavorCombo<S>> result = Lists.newArrayList();
         if (flavorDimensions == null || flavorDimensions.isEmpty()) {
-            for (ProductFlavor flavor : productFlavors) {
-                result.add(new ProductFlavorCombo(ImmutableList.of(flavor)));
+            for (S flavor : productFlavors) {
+                result.add(new ProductFlavorCombo<S>(ImmutableList.of(flavor)));
             }
         } else {
             // need to group the flavor per dimension.
             // First a map of dimension -> list(ProductFlavor)
-            ArrayListMultimap<String, ProductFlavor> map = ArrayListMultimap.create();
-            for (ProductFlavor flavor : productFlavors) {
+            ArrayListMultimap<String, S> map = ArrayListMultimap.create();
+            for (S flavor : productFlavors) {
                 String flavorDimension = flavor.getDimension();
 
                 if (flavorDimension == null) {
@@ -108,7 +113,7 @@ public class ProductFlavorCombo {
             }
 
             createProductFlavorCombinations(result,
-                    new ProductFlavor[flavorDimensions.size()],
+                    Lists.<S>newArrayListWithCapacity(flavorDimensions.size()),
                     0, flavorDimensions, map);
         }
         return result;
@@ -127,14 +132,14 @@ public class ProductFlavorCombo {
         return builder.build();
     }
 
-    private static void createProductFlavorCombinations(
-            List<ProductFlavorCombo> flavorGroups,
-            ProductFlavor[] group,
+    private static <S extends DimensionAware & Named> void createProductFlavorCombinations(
+            List<ProductFlavorCombo<S>> flavorGroups,
+            List<S> group,
             int index,
             List<String> flavorDimensionList,
-            ListMultimap<String, ProductFlavor> map) {
+            ListMultimap<String, S> map) {
         if (index == flavorDimensionList.size()) {
-            flavorGroups.add(new ProductFlavorCombo(filterNullFromArray(group)));
+            flavorGroups.add(new ProductFlavorCombo<S>(Iterables.filter(group, Predicates.notNull())));
             return;
         }
 
@@ -143,7 +148,7 @@ public class ProductFlavorCombo {
         String dimension = flavorDimensionList.get(index);
 
         // from our map, get all the possible flavors in that dimension.
-        List<ProductFlavor> flavorList = map.get(dimension);
+        List<S> flavorList = map.get(dimension);
 
         // loop on all the flavors to add them to the current index and recursively fill the next
         // indices.
@@ -151,10 +156,11 @@ public class ProductFlavorCombo {
             throw new RuntimeException(String.format(
                     "No flavor is associated with flavor dimension '%1$s'.", dimension));
         } else {
-            for (ProductFlavor flavor : flavorList) {
-                group[index] = flavor;
+            for (S flavor : flavorList) {
+                group.add(flavor);
                 createProductFlavorCombinations(
                         flavorGroups, group, index + 1, flavorDimensionList, map);
+                group.remove(group.size() - 1);
             }
         }
     }

build-system/gradle-experimental/src/main/groovy/com/android/build/gradle/managed/ProductFlavor.java
@@ -20,6 +20,7 @@ import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.builder.model.AndroidArtifact;
 import com.android.builder.model.AndroidProject;
+import com.android.builder.model.DimensionAware;
 import com.android.builder.model.Variant;
 
 import org.gradle.api.Named;
@@ -31,17 +32,13 @@ import java.io.File;
 import java.util.List;
 import java.util.Set;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.Map;
-
 /**
  * A Managed product flavor.
  *
  * TODO: Convert Unmanaged Collection to Managed type when Gradle provides ModelSet for basic class.
  */
 @Managed
-public interface ProductFlavor extends Named {
+public interface ProductFlavor extends Named, DimensionAware {
 
     /**
      * Map of Build Config Fields where the key is the field name.
@@ -132,6 +129,7 @@ public interface ProductFlavor extends Named {
     /**
      * Returns the flavor dimension or null if not applicable.
      */
+    @Override
     @Nullable
     String getDimension();
     void setDimension(String dimension);
@@ -282,5 +280,5 @@ public interface ProductFlavor extends Named {
     Boolean getUseJack();
     void setUseJack(Boolean useJack);
 
-    NdkConfig getNdkConfig();
+    NdkOptions getNdk();
 }

build-system/gradle-experimental/src/main/groovy/com/android/build/gradle/ndk/internal/NdkConfiguration.java
@@ -77,19 +77,6 @@ public class NdkConfiguration {
                         getCCompiler(binary).define("ANDROID_NDK");
                         getCppCompiler(binary).define("ANDROID_NDK");
 
-                        // TODO: Right now, we create the library in the "obj" folder, then strip
-                        // the debug symbols and place the final output in the "lib" folder.  For
-                        // non-debuggable build, we should just place the output in the lib folder.
-
-                        // Set output library filename.
-                        binary.setSharedLibraryFile(
-                                new File(
-                                        buildDir,
-                                        NdkNamingScheme.getDebugLibraryDirectoryName(binary)
-                                                + "/"
-                                                + NdkNamingScheme.getSharedLibraryFileName(
-                                                        ndkConfig.getModuleName())));
-
                         // Replace output directory of compile tasks.
                         binary.getTasks().withType(CCompile.class, new Action<CCompile>() {
                             @Override
@@ -124,44 +111,66 @@ public class NdkConfiguration {
                         binary.getLinker().args("--sysroot=" + sysroot);
                         binary.getLinker().args("-Wl,--build-id");
 
-                        if (ndkConfig.getRenderscriptNdkMode()) {
-                            getCCompiler(binary).args("-I" + sysroot + "/usr/include/rs");
-                            getCCompiler(binary).args("-I" + sysroot + "/usr/include/rs/cpp");
-                            getCppCompiler(binary).args("-I" + sysroot + "/usr/include/rs");
-                            getCppCompiler(binary).args("-I" + sysroot + "/usr/include/rs/cpp");
-                            binary.getLinker().args("-L" + sysroot + "/usr/lib/rs");
-                        }
-
-                        NativeToolSpecificationFactory.create(
-                                ndkHandler,
-                                binary.getBuildType(),
-                                binary.getTargetPlatform()).apply(binary);
-
-                        // Add flags defined in NdkConfig
-                        for (String flag : ndkConfig.getCFlags()) {
-                            getCCompiler(binary).args(flag);
-                        }
-
-                        for (String flag : ndkConfig.getCppFlags()) {
-                            getCppCompiler(binary).args(flag);
-                        }
-
-                        for (String flag : ndkConfig.getLdFlags()) {
-                            binary.getLinker().args(flag);
-                        }
-
-                        for (String ldLib : ndkConfig.getLdLibs()) {
-                            binary.getLinker().args("-l" + ldLib);
-                        }
-
-                        StlNativeToolSpecification stlConfig = new StlNativeToolSpecification(
-                                ndkHandler,
-                                ndkConfig.getStl(),
-                                binary.getTargetPlatform());
-                        stlConfig.apply(binary);
                     }
 
                 });
+    }
+
+    /**
+     * Configure native binary with variant specific options.
+     */
+    public static void configureBinary(
+            SharedLibraryBinarySpec binary,
+            final File buildDir,
+            final NdkConfig ndkConfig,
+            final NdkHandler ndkHandler) {
+        // Set output library filename.
+        binary.setSharedLibraryFile(
+                new File(
+                        buildDir,
+                        NdkNamingScheme.getDebugLibraryDirectoryName(binary)
+                                + "/"
+                                + NdkNamingScheme.getSharedLibraryFileName(
+                                ndkConfig.getModuleName())));
+
+        String sysroot = ndkHandler.getSysroot(
+                Abi.getByName(binary.getTargetPlatform().getName()));
+
+        if (ndkConfig.getRenderscriptNdkMode()) {
+            getCCompiler(binary).args("-I" + sysroot + "/usr/include/rs");
+            getCCompiler(binary).args("-I" + sysroot + "/usr/include/rs/cpp");
+            getCppCompiler(binary).args("-I" + sysroot + "/usr/include/rs");
+            getCppCompiler(binary).args("-I" + sysroot + "/usr/include/rs/cpp");
+            binary.getLinker().args("-L" + sysroot + "/usr/lib/rs");
+        }
+
+        NativeToolSpecificationFactory.create(
+                ndkHandler,
+                binary.getBuildType(),
+                binary.getTargetPlatform()).apply(binary);
+
+        // Add flags defined in NdkConfig
+        for (String flag : ndkConfig.getCFlags()) {
+            getCCompiler(binary).args(flag);
+        }
+
+        for (String flag : ndkConfig.getCppFlags()) {
+            getCppCompiler(binary).args(flag);
+        }
+
+        for (String flag : ndkConfig.getLdFlags()) {
+            binary.getLinker().args(flag);
+        }
+
+        for (String ldLib : ndkConfig.getLdLibs()) {
+            binary.getLinker().args("-l" + ldLib);
+        }
+
+        StlNativeToolSpecification stlConfig = new StlNativeToolSpecification(
+                ndkHandler,
+                ndkConfig.getStl(),
+                binary.getTargetPlatform());
+        stlConfig.apply(binary);
     }
 
     public static void createTasks(ModelMap<Task> tasks, SharedLibraryBinarySpec binary,