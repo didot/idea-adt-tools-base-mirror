tadfisher/platform_tools_base

Fixing perflib to only count referent field as soft reference.
Only the referent field in a soft reference is considered the soft
link for dominator and shortest visitor computations.

Change-Id: Idf1eb65deacf02cbf7c807a3b55a6506737e0f93

perflib/src/main/java/com/android/tools/perflib/heap/analysis/ShortestDistanceVisitor.java
@@ -35,7 +35,10 @@ public class ShortestDistanceVisitor extends NonRecursiveVisitor {
     @Override
     public void visitLater(Instance parent, @NonNull Instance child) {
         if (mVisitDistance < child.getDistanceToGcRoot() &&
-                (parent == null || !parent.getIsSoftReference() || child.getIsSoftReference())) {
+                (parent == null ||
+                     parent.getSoftReferences() == null ||
+                     !parent.getSoftReferences().contains(child) ||
+                     child.getIsSoftReference())) {
             child.setDistanceToGcRoot(mVisitDistance);
             child.setNextInstanceToGcRoot(mPreviousInstance);
             mPriorityQueue.add(child);

didot/idea-adt-tools-base-mirror

perflib/src/main/java/com/android/tools/perflib/heap/analysis/ShortestDistanceVisitor.java
@@ -35,7 +35,10 @@ public class ShortestDistanceVisitor extends NonRecursiveVisitor {
     @Override
     public void visitLater(Instance parent, @NonNull Instance child) {
         if (mVisitDistance < child.getDistanceToGcRoot() &&
-                (parent == null || !parent.getIsSoftReference() || child.getIsSoftReference())) {
+                (parent == null ||
+                     child.getSoftReferences() == null ||
+                     !child.getSoftReferences().contains(parent) ||
+                     child.getIsSoftReference())) {
             child.setDistanceToGcRoot(mVisitDistance);
             child.setNextInstanceToGcRoot(mPreviousInstance);
             mPriorityQueue.add(child);