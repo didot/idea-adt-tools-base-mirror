121600c795ac22d76f853bd404cb7a90f473eb8a
Yann Cébron <yann.cebron@jetbrains.com>
2018-11-21 11:46:22 +0100

    DebugKeyProviderTest: super.tearDown() in finally

--- sdklib/src/test/java/com/android/sdklib/internal/build/DebugKeyProviderTest.java
+++ sdklib/src/test/java/com/android/sdklib/internal/build/DebugKeyProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,12 +41,17 @@ public class DebugKeyProviderTest extends TestCase {
 
     @Override
     protected void tearDown() throws Exception {
-        super.tearDown();
-        if (mTmpFile != null) {
-            if (!mTmpFile.delete()) {
-                mTmpFile.deleteOnExit();
+        try {
+            if (mTmpFile != null) {
+                if (!mTmpFile.delete()) {
+                    mTmpFile.deleteOnExit();
+                }
             }
+        }
+        finally {
             mTmpFile = null;
+
+            super.tearDown();
         }
     }
 
