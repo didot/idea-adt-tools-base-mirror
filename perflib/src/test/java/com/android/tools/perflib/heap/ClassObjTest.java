3dd217c56e41ce70ed88449b2248ef5c46a4645c
Yann Cébron <yann.cebron@jetbrains.com>
2018-11-21 11:41:10 +0100

    ClassObjTest: super.tearDown() in finally

--- perflib/src/test/java/com/android/tools/perflib/heap/ClassObjTest.java
+++ perflib/src/test/java/com/android/tools/perflib/heap/ClassObjTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,10 +37,14 @@ public class ClassObjTest extends TestCase {
 
   @Override
   protected void tearDown() throws Exception {
-    super.tearDown();
+    try {
+      mSnapshot.dispose();
+    }
+    finally {
+      mSnapshot = null;
 
-    mSnapshot.dispose();
-    mSnapshot = null;
+      super.tearDown();
+    }
   }
 
   public void testGetAllFieldsCount() {
