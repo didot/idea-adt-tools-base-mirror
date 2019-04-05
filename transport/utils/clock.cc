didot/idea-adt-tools-base-mirror

commit 4b2afc19bcdba5494c9647a8255ef13a5e19bd94

Minor refactor: removing int64->double->int64 conversion.
Test: manual
Bug: none

Change-Id: I39df76c4dcd3592003256c7bb1ea897174894161

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1e9 * time.tv_sec + time.tv_nsec;
+  return 1000000000 * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler

commit b9061a8a085e29bf2c9791a1dc255df07bc10eac

Fix SteadyClock int overflow on 32-bit platforms
Broken by 4b2afc19bcdba5494c9647a8255ef13a5e19bd94

Bug: N/A
Test: N/A
Change-Id: I6d6697fbafb333d689b8795be020fbba0952b0f2

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1000000000 * time.tv_sec + time.tv_nsec;
+  return static_cast<int64_t>(1000000000) * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler